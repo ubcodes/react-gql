de821725cb40299697766fe980b7fbcea841f9ec
UB30-4 <101749091+UB30-4@users.noreply.github.com>
2023-08-17 00:52:14 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from "@apollo/client"
+
+// https://admin-hs5d6.ondigitalocean.app/graphql
+// https://rickandmortyapi.com/graphql
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
